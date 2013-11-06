7c7cc82f463782dbbfe5910c014e97a6b0aec471
milankamilya <innofied@Sandips-Mac-mini-2.local>
2013-11-06 12:49:25 +0530

    initial push

---
+++ ImageGallery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  SettingAppDelegate.h
+//  ImageGallery
+//
+//  Created by Sandip Saha on 10/10/13.
+//  Copyright (c) 2013 Sandip Saha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) NSArray *imageArrayOfAssetLibrary;
+
+@end
